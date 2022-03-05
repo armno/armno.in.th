6717d91d03b44f2ae1b6ec46f04d80492d9c0da6
Armno <monkeyarmno@gmail.com>
2022-03-05 19:37:28 +0700

    addd favicon

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            a: {
+              textUnderlineOffset: '1px'
+            }
+          }
+        }
+      },
+      fontFamily: {
+        sans: ['Sarabun', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  },
   content: [
     './public/**/*.html',
     './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}'
