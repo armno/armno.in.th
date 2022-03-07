d10ef7ffb2aa584995eb52f7af2fc0bb0620c8cc
Armno <monkeyarmno@gmail.com>
2022-03-07 22:26:02 +0700

    fix images in canyon post

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -22,7 +22,7 @@ module.exports = {
   },
   content: [
     './public/**/*.html',
-    './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}'
+    './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue,md}'
   ],
   plugins: [
     require('@tailwindcss/typography')
