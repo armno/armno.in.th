46be74561f5064053521db311b794456109ee00c
Armno <monkeyarmno@gmail.com>
2022-08-27 23:03:16 +0700

    add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://armno.in.th',
-  integrations: [sitemap()]
+  integrations: [sitemap(), mdx()]
 });
