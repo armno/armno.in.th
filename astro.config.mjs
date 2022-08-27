65e9bf51afb82d33057732d85ab5b0825ec62770
Armno <monkeyarmno@gmail.com>
2022-08-27 23:46:22 +0700

    add netlify cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,34 @@ import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 
 import mdx from "@astrojs/mdx";
+import NetlifyCMS from 'astro-netlify-cms';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://armno.in.th',
-  integrations: [sitemap(), mdx()]
+  integrations: [sitemap(), mdx(),
+  NetlifyCMS({
+    config: {
+      backend: {
+        name: 'git-gateway',
+        branch: 'main',
+      },
+      collections: [
+        // Content collections
+        {
+          name: 'blog',
+          label: 'Blog posts',
+          folder: 'src/pages/blog',
+          fields: [
+            {
+              name: 'title',
+              widget: 'string',
+              label: 'Post title'
+            }
+          ]
+        }
+      ],
+    },
+  }),
+  ]
 });
