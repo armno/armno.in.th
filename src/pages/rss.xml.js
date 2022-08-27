c0a21aa4c45f7d5b6d0936a1e059fca7e5a2a4c2
Armno <monkeyarmno@gmail.com>
2022-08-27 23:30:57 +0700

    convert .md to .mdx

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,6 +4,6 @@ export const get = () => rss({
   title: `Armno's blog`,
   description: 'Notes from my thoughts and learnings.',
   site: import.meta.env.SITE,
-  items: import.meta.glob('./**/*.md'),
+  items: import.meta.glob('./**/*.mdx'),
   stylesheet: '/rss/pretty-feed-v3.xsl'
 });
