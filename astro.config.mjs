9393363c2e568dcb6c5555e56a68cca98734d040
Armno <monkeyarmno@gmail.com>
2022-11-28 10:40:16 +0700

    update dependencies and tailwind setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 // https://docs.astro.build/reference/configuration-reference
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
-
 import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://armno.in.th',
-  integrations: [sitemap(), mdx()]
+  integrations: [sitemap(), mdx(), tailwind()]
 });
