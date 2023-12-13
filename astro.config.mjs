0db0b2f703f2c0678779aaa6ea90c8f5069d0502
Armno Prommarak <monkeyarmno@gmail.com>
2023-12-13 21:53:08 +0700

    WIP: setup keystatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,15 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
+import keystatic from '@keystatic/astro'
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://armno.in.th",
-  integrations: [
-    sitemap(),
-    mdx(),
-    tailwind(),
-  ],
+  output: "hybrid",
+  integrations: [sitemap(), mdx(), tailwind(), react(), markdoc(), keystatic()]
 });
