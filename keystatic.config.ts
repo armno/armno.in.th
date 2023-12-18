73c9fe539edb7802a1790327e89f81a1b5d18a05
Armno Prommarak <monkeyarmno@gmail.com>
2023-12-18 18:27:52 +0700

    WIP: trying things

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -9,10 +9,14 @@ export default config({
     posts: collection({
       label: 'Posts',
       slugField: 'title',
-      path: 'src/content/blog/*',
+      path: 'src/content/blog/*/',
       format: { contentField: 'content' },
       schema: {
         title: fields.slug({ name: { label: 'Title' } }),
+        pubDate: fields.text({ label: 'Published Date' }),
+        description: fields.text({ label: 'Description' }),
+        published: fields.checkbox({ label: 'Published' }),
+        thumbnail: fields.image({ label: 'Thumbnail' }),
         content: fields.document({
           label: 'Content',
           formatting: true,
