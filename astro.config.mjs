c4152c2778a30678a31816c55eb3d5c60b3161f1
Armno P <monkeyarmno@gmail.com>
2023-12-26 17:50:33 +0700

    try to disable keystatic on prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,6 @@ import netlify from "@astrojs/netlify/functions";
 export default defineConfig({
   site: "https://armno.in.th",
   output: "hybrid",
-  integrations: [sitemap(), mdx(), tailwind(), react(), markdoc(), keystatic()],
+  integrations: [sitemap(), mdx(), tailwind(), react(), markdoc(), process.env.NODE_ENV === 'production' ? null : keystatic()],
   adapter: netlify()
 });
