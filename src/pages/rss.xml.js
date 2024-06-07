9e5d276ef5ff48f031e0d7dfc3d3cb9969d378aa
Armno P <monkeyarmno@gmail.com>
2024-06-07 14:52:55 +0700

    fix wrong url of articles in sitemap

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -12,7 +12,7 @@ export async function GET(context) {
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
-      link: `/posts/${post.slug}/`,
+      link: `/blog/${post.slug}/`,
     })),
     stylesheet: '/rss/pretty-feed-v3.xsl'
   });
